a764f1ef6f6914ea11945b0706b5a4e33ccf1aed
Eude Lesperance <l.keude96@gmail.com>
2016-09-19 11:47:11 -0500

    Initial Commit

---
+++ CODE2040APIChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CODE2040APIChallenge
+//
+//  Created by Eude Lesperance on 9/19/16.
+//  Copyright © 2016 Eude Lesperance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
